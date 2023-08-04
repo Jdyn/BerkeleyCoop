4abb961ce449b2e09f1f1a003a4d8695b95fc416
Jaden <moorejaden5389@gmail.com>
2023-08-04 01:29:07 -0700

    add svg support

--- client/src/screens/Events/EventCard/EventCard.tsx
+++ client/src/screens/Events/EventCard/EventCard.tsx
@@ -1,11 +1,21 @@
 import styles from "./EventCard.module.css";
-
+import { ReactComponent as EventImage } from '../../../images/event.svg'
 interface Props {
   event: any;
 }
 
-const EventCard = (props: Props) => {
-  return <div className={styles.root}></div>;
+const EventCard = ({event}: Props) => {
+
+  return <div className={styles.root}>
+		<EventImage />
+		<div className={styles.container}>
+			<h2>Event</h2>
+			<h4>{event.title}</h4>
+			<p>
+				{event.description}
+			</p>
+		</div>
+	</div>;
 };
 
 export default EventCard;
--- client/vite.config.ts
+++ client/vite.config.ts
@@ -1,11 +1,17 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react-swc";
+import svgr from "vite-plugin-svgr";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-	server: {
-		port: 3000,
-		host: 'localhost'
-	}
-})
+  plugins: [
+    react(),
+    svgr({
+      include: "**/*.svg",
+    }),
+  ],
+  server: {
+    port: 3000,
+    host: "localhost",
+  },
+});
